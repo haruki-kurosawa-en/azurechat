5ffef0bdf7909b3bccd5f3dd964c20dab478b82d
haruki-kurosawa-en <haruki_kurosawa@en-japan.com>
2023-11-24 19:43:10 +0900

    スタートページの日本語化

--- src/features/chat/chat-ui/chat-empty-state/start-new-chat.tsx
+++ src/features/chat/chat-ui/chat-empty-state/start-new-chat.tsx
@@ -17,12 +17,10 @@ export const StartNewChat: FC<Prop> = (props) => {
           {AI_NAME}
         </Typography>
         <div className="flex flex-col gap-2">
-          <p className="">
-            Welcome to {AI_NAME}. You should interact in a friendly manner with
-            the AI assistant and refrain from participating in any harmful
-            activities.
-          </p>
-          <p>You can start a new chat with me by clicking the button below.</p>
+        <p className="">
+          {AI_NAME}にようこそ。AIアシスタントと気軽に仲良くやりとりしましょう。
+        </p>
+        <p>下のボタンを押せば、いつでも新しいチャットを始められますよ。</p>
         </div>
         <div className="-mx-5 -mb-5 p-5 flex flex-col border-t bg-muted">
           <NewChat />
